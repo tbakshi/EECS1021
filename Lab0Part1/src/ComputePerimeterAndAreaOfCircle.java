99db81b283d59074d2b8ed497c19a7e757177149
bakshi-tania-215399462 <tbakshi@my.yorku.ca>
2019-01-07 23:31:30 -0500

    Radius and Minutes Converter created
    Two classes created, one computed preimeter of a circle and radius and one converting seconds into minutes

---
+++ Lab0Part1/src/ComputePerimeterAndAreaOfCircle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ComputePerimeterAndAreaOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner readin = new Scanner(System.in);
+		System.out.println("Enter an integer for radius:");
+		int radius = Integer.parseInt(readin.nextLine());
+		double perimeter = 2*3.14*radius; 
+		double area = radius*radius*2*3.14;
+		
+		System.out.println("What's your name?"); 
+		String name = readin.nextLine(); 
+		
+		System.out.println(name +", your input circle with radius " + radius +" has perimeter " + perimeter + " and area " + area );
+		
+	}
+
+}
---
+++ Lab0Part1/src/ConvertSecondsIntoMinutes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ConvertSecondsIntoMinutes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner readin = new Scanner(System.in);
+		System.out.println("What's your name?");
+		String name = readin.nextLine(); 
+		
+		System.out.println("Enter an integer for seconds:");
+		int seconds = Integer.parseInt(readin.nextLine());
+		int minutes = seconds/60; 
+		int secondsModulo = seconds%60;
+		
+		System.out.print(name + ", your input "+ seconds +" seconds can be divided into "+ minutes + " minutes and " + secondsModulo + " seconds");
+		
+	}
+
+}
