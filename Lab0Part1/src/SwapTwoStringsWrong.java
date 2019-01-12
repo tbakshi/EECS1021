29699244831dc5f376019ccd4a7d7e4817b62a5c
bakshi-tania-215399462 <tbakshi@my.yorku.ca>
2019-01-12 12:32:29 -0500

    update

--- Lab0Part1/src/SwapTwoStringsWrong.java
+++ Lab0Part1/src/SwapTwoStringsWrong.java
@@ -3,7 +3,21 @@ public class SwapTwoStringsWrong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String person1 = "Alan";
+		String person2 = "Tom";
+		System.out.print("Before the swap:");
+		System.out.print("person1 is " + person1+ " and ");
+		System.out.println("person2 is " +person2+".");
+		person1 = person2;
+		person2 = person1;
+		System.out.print("After the swap:");
+		System.out.print("person1 is " + person1+ " and ");
+		System.out.println("person2 is " +person2+".");
+		int x = 4;
+		int y = 5;
+		int tempInt = x;
+		x = y;
+		y = tempInt;
 	}
 
 }
